3597580f743cf7076f0c31b137531127a16db4b7
BeeMargarida <t-ms@platforme.com>
2020-07-02 19:19:21 +0100

    Attacks and GUI

--- Entities/Player/Player.gd
+++ Entities/Player/Player.gd
@@ -3,6 +3,64 @@ extends KinematicBody2D
 # Player movement speed
 export var speed = 75
 
+# Animation variables
+var last_direction = Vector2(0, 1)
+var attack_playing = false
+
+# Player stats
+var health = 100
+var health_max = 100
+var health_regeneration = 1
+var mana = 100
+var mana_max = 100
+var mana_regeneration = 2
+
+signal player_stats_changed
+
+func get_animation_direction(direction: Vector2):
+	var norm_direction = direction.normalized()
+	if norm_direction.y >= 0.707:
+		return "down"
+	elif norm_direction.y <= -0.707:
+		return "up"
+	elif norm_direction.x <= -0.707:
+		return "left"
+	elif norm_direction.x >= 0.707:
+		return "right"
+	return "down"
+
+func animates_player(direction: Vector2):
+	if direction != Vector2.ZERO:
+		# update last_direction
+		last_direction = 0.5 * last_direction + 0.5 * direction
+		
+		# Choose walk animation based on movement direction
+		var animation = get_animation_direction(last_direction) + "_walk"
+		
+		# Play the walk animation
+		$Sprite.frames.set_animation_speed(animation, 2 + 8 * direction.length())
+		$Sprite.play(animation)
+	else:
+		# Choose idle animation based on last movement direction and play it
+		var animation = get_animation_direction(last_direction) + "_idle"
+		$Sprite.play(animation)
+
+func _on_Sprite_animation_finished():
+	attack_playing = false
+
+func _input(event):
+	if event.is_action_pressed("attack"):
+		attack_playing = true
+		var animation = get_animation_direction(last_direction) + "_attack"
+		$Sprite.play(animation)
+	elif event.is_action_pressed("fireball"):
+		if mana >= 25:
+			mana = mana - 25
+			emit_signal("player_stats_changed", self)
+			attack_playing = true
+			var animation = get_animation_direction(last_direction) + "_fireball"
+			$Sprite.play(animation)
+
 func _physics_process(delta):
 	# Get player input
 	var direction: Vector2
@@ -15,4 +73,27 @@ func _physics_process(delta):
 		
 	# Apply movement
 	var movement = speed * direction * delta
+	if attack_playing:
+		movement = 0.3 * movement
+
 	move_and_collide(movement)
+	
+	if not attack_playing:
+		# Animate player based on direction
+		animates_player(direction)
+
+func _process(delta):
+	# Regenerates mana
+	var new_mana = min(mana + mana_regeneration * delta, mana_max)
+	if new_mana != mana:
+		mana = new_mana
+		emit_signal("player_stats_changed", self)
+
+	# Regenerates health
+	var new_health = min(health + health_regeneration * delta, health_max)
+	if new_health != health:
+		health = new_health
+		emit_signal("player_stats_changed", self)
+
+func _ready():
+	emit_signal("player_stats_changed", self)
